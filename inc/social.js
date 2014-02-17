d690530d86a782a51e03bcd924b17f3de78ca86f
Maxim Penzin <maxp@ya.ru>
2014-02-17 17:03:48 +0900

    social: twitter button

--- inc/social.js
+++ inc/social.js
@@ -13,6 +13,16 @@ $(function() {
         VK.Widgets.Like("vk_like", {type: "mini", height: 20});
     });
 
+    !function(d,s,id){
+        var js, 
+            fjs=d.getElementsByTagName(s)[0],
+            p=/^http:/.test(d.location)?'http':'https';
+            if(!d.getElementById(id)){
+                js=d.createElement(s); js.id=id;
+                js.src=p+'://platform.twitter.com/widgets.js';
+                fjs.parentNode.insertBefore(js,fjs);
+    }}(document, 'script', 'twitter-wjs');
+
     /*
     !function(d, id, did, st) {
         var js = d.createElement("script");
